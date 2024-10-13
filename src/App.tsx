099dfd5771c40889f5d02318864ce33c5447d46a
yasminMVieira <minmarques2001@gmail.com>
2024-10-12 23:06:57 -0300

    HashRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Sidebarr from './components/Sidebarr';
 import Home from './pages/Home';
 import Register from './pages/Register';
